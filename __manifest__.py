8f9d5def049f399983beab19bcd721b41058dcae
saranakulina-odoo <andyut@indoguna.co.id>
2017-04-24 18:32:56 +0700

    Add files via upload
    Init Py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Web Disable Export Clients',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'description': """Only Show Export for Admin""",
+    'depends': ['web'],
+    'data': [
+        'view/disable_export_view.xml',
+    ],
+    'application': True,
+    'sequence': 0,
+    'price': '0',
+    "currency": 'EUR',
+    'images': [],
+    'support': 'thanhchatvn@gmail.com'
+}
